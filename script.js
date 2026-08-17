804a1d0b448173878a4db6fd0a47627302a3c9bb
khalidramdaoui530-art <khalidramdaoui530@gmail.com>
2026-08-17 14:30:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Mobile Menu
 
-const menuBtn = document.getElementById("menu-toggle");
-const navbar = document.getElementById("navbar");
-
-menuBtn.addEventListener("click", () => {
-
-navbar.classList.toggle("active");
-
-});
